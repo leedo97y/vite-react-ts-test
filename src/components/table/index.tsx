39958810277f8f3f6c6c09da01e045800ea17dda
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-08-02 15:56:21 +0900

    fix: fix type error

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -139,7 +139,7 @@ const CustomPaginationActionsTable: React.FC = () => {
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
   const handleChangePage = (
-    event: React.MouseEvent<HTMLButtonElement> | null,
+    _event: React.MouseEvent<HTMLButtonElement> | null,
     newPage: number
   ) => {
     setPage(newPage);
